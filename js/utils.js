2609ce16895301f626e2d860c310556b48db2e03
wangzwal <wangzwal@tmlake.com>
2024-12-19 10:28:26 +0800

    Site updated: 2024-12-19 10:28:26

--- js/utils.js
+++ js/utils.js
@@ -245,7 +245,8 @@ NexT.utils = {
           scrollTop: offset + 10
         });
       });
-      return document.getElementById(link.getAttribute('href').replace('#', ''));
+      
+      return document.getElementById(link.getAttribute('href') && link.getAttribute('href').replace('#', ''));
     });
 
     var tocElement = document.querySelector('.post-toc-wrap');
